f9d8719031caeded19a326c820e996784c01930e
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-07-19 19:59:46 +0900

    [Gold V] Title: 토마토, Time: 884 ms, Memory: 140248 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,41 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
-
-class Tomato {
-	int x;
-	int y;
-	int z;
-	
-	public Tomato(int x, int y, int z){
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
-}
+import java.util.StringTokenizer;
 
 public class Main {
 	static int[] dx = {-1, 1, 0, 0, 0, 0};
 	static int[] dy = {0, 0 ,-1, 1, 0, 0};
 	static int[] dz = {0, 0, 0, 0, -1, 1};
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	
+	static class Tomato {
+		int x;
+		int y;
+		int z;
 		
-		int M = sc.nextInt();
-		int N = sc.nextInt();
-		int H = sc.nextInt();
+		Tomato(int x, int y, int z){
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
 		
 		int[][][] box = new int[H][N][M];
 		
 		Queue<Tomato> q = new LinkedList<Tomato>();
+		int tomatoCnt = 0;
+		int emptyCnt = 0;
+		int ans = 1;
 		
 		for(int i = 0; i < H; i++) {
 			for(int j = 0; j < N; j++) {
+				String[] row = br.readLine().split(" ");
 				for(int k = 0; k < M; k++) {
-					box[i][j][k] = sc.nextInt();
+					box[i][j][k] = Integer.parseInt(row[k]);
 					if(box[i][j][k] == 1) {
 						q.add(new Tomato(i, j, k));
+						tomatoCnt++;
+					}
+					else if(box[i][j][k] == -1) {
+						emptyCnt++;
 					}
 				}
 			}
@@ -51,31 +63,20 @@ public class Main {
 				
 				if(0 <= nx && nx < H && 0 <= ny && ny < N && 0 <= nz && nz < M) {
 					if(box[nx][ny][nz] == 0) {
-						box[nx][ny][nz] = box[now.x][now.y][now.z] + 1;
+						ans = box[now.x][now.y][now.z] + 1;
+						box[nx][ny][nz] = ans;
+						tomatoCnt++;
 						q.add(new Tomato(nx, ny, nz));
 					}
 				}
 			}
 		}
 		
-		int ans = 0;
-		
-		Loop1: for(int i = 0; i < H; i++) {
-			for(int j = 0; j < N; j++) {
-				for(int k = 0; k < M; k++) {
-					if(box[i][j][k] == 0) {
-						System.out.println(-1);
-						ans = -1;
-						break Loop1;
-					}
-					if(box[i][j][k] > ans) {
-						ans = box[i][j][k];
-					}
-				}
-			}
-		}
-		if(ans != -1) {
+		if(tomatoCnt == (H*N*M) - emptyCnt) {
 			System.out.println(ans-1);
+		}
+		else {
+			System.out.println(-1);
 		}
 	}
 
