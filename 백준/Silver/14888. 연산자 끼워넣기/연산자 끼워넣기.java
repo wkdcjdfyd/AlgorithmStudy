7743096ce8523448efabd3efda6697824a78078e
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-07-21 20:18:37 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 508 ms, Memory: 13940 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,17 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static Queue<int[]> q = new LinkedList<>();
+	static Integer[] nums;
+	static int max = Integer.MIN_VALUE;
+	static int min = Integer.MAX_VALUE;
 	
 	static void permutation(int[] arr, int[] output, boolean[] visited, int depth, int n, int r) {
 		if(depth == r) {
-			q.add(Arrays.copyOf(output, output.length));
+			calc(output);
 			return;
 		}
 		for(int i = 0; i < n; i++) {
@@ -24,19 +23,46 @@ public class Main {
 			}
 		}
 	}
+	
+	public static void calc(int[] nowOp) {
+		int result = nums[0];
+		int nowIdx = 1;
+		
+		for(int nxt: nowOp) {
+			if(nxt == 0) {
+				result += nums[nowIdx++];
+			}
+			else if(nxt == 1) {
+				result -= nums[nowIdx++];
+			}
+			else if(nxt == 2) {
+				result *= nums[nowIdx++];
+			}
+			else {
+				result /= nums[nowIdx++];
+			}
+		}
+		
+		if(result > max) {
+			max = result;
+		}
+		if(result < min) {
+			min = result;
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		int N = Integer.parseInt(br.readLine());
-		Integer[] arr = new Integer[N];
+		nums = new Integer[N];
 		int[] op = new int[N-1];
 		int idx = 0;
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		st = new StringTokenizer(br.readLine());
@@ -48,37 +74,6 @@ public class Main {
 		}
 		
 		permutation(op, new int[N-1], new boolean[N-1], 0, N-1, N-1);
-		
-		int max = Integer.MIN_VALUE;
-		int min = Integer.MAX_VALUE;
-		
-		while(!q.isEmpty()) {
-			int temp = arr[0];
-			int nowIdx = 1;
-			int[] now = q.poll();
-			
-			for(int nxt: now) {
-				if(nxt == 0) {
-					temp += arr[nowIdx++];
-				}
-				else if(nxt == 1) {
-					temp -= arr[nowIdx++];
-				}
-				else if(nxt == 2) {
-					temp *= arr[nowIdx++];
-				}
-				else {
-					temp /= arr[nowIdx++];
-				}
-			}
-			
-			if(temp > max) {
-				max = temp;
-			}
-			if(temp < min) {
-				min = temp;
-			}
-		}
 		
 		System.out.println(max);
 		System.out.println(min);
