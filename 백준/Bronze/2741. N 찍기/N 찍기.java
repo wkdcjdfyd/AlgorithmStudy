e2a994a6f9f7a97db20468a8823872985d16c42e
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-07-21 20:37:24 +0900

    [Bronze V] Title: N 찍기, Time: 208 ms, Memory: 29684 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i < n+1; i++) {
+			sb.append(i + "\n");
+		}
+		System.out.print(sb.toString());
+	}
+
+}
