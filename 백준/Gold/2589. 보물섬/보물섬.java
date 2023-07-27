20ecc4715654b7f75d073b2fa0dde2fc8dca32b1
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-07-27 21:04:13 +0900

    [Gold V] Title: 보물섬, Time: 520 ms, Memory: 292664 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.java
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -10,33 +10,42 @@ public class Main {
 	static int[] dy = {0, 0, -1, 1};
 	static int N;
 	static int M;
-	static int[][] graph;
-	static Queue<int[]> q = new LinkedList<>();
+	static char[][] graph;
 	static int ans;
+	
+	static class Land {
+		int x;
+		int y;
+		
+		public Land(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 	
 	public static void bfs(int x, int y) {
 		int[][] visited = new int[N][M];
-		Queue<int[]> bfsQ = new LinkedList<>();
-		bfsQ.add(new int[] {x, y});
+		Queue<Land> q = new LinkedList<>();
+		q.add(new Land(x, y));
 		visited[x][y] = 1;
 		int max = 0;
 		
-		while(!bfsQ.isEmpty()) {
-			int[] now = bfsQ.poll();
+		while(!q.isEmpty()) {
+			Land now = q.poll();
 			
 			for(int i = 0; i < 4; i++) {
-				int nx = now[0] + dx[i];
-				int ny = now[1] + dy[i];
+				int nx = now.x + dx[i];
+				int ny = now.y + dy[i];
 				
 				if(nx < 0 || ny < 0 || nx >= N || ny >= M) {
 					continue;
 				}
-				if(graph[nx][ny] == -1 || visited[nx][ny] > 0) {
+				if(graph[nx][ny] == 'W' || visited[nx][ny] > 0) {
 					continue;
 				}
-				visited[nx][ny] = visited[now[0]][now[1]] + 1;
-				max = visited[now[0]][now[1]] + 1;
-				bfsQ.add(new int[] {nx, ny});
+				visited[nx][ny] = visited[now.x][now.y] + 1;
+				max = visited[now.x][now.y] + 1;
+				q.add(new Land(nx, ny));
 			}
 		}
 		if(ans < max) {
@@ -44,29 +53,26 @@ public class Main {
 		}
 	}
 
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		graph = new int[N][M];
+		graph = new char[N][M];
 		
 		for(int i = 0; i < N; i++) {
-			String s = br.readLine();
-			for(int j = 0; j < M;j++) {
-				if(s.charAt(j) == 'W') {
-					graph[i][j] = -1;
-				}
-				else {
-					graph[i][j] = 0;
-					q.add(new int[] {i, j});
+			graph[i] = br.readLine().toCharArray();
+		}
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				if(graph[i][j] == 'L') {
+					bfs(i, j);
 				}
 			}
 		}
-		while(!q.isEmpty()) {
-			int[] now = q.poll();
-			bfs(now[0], now[1]);
-		}
+		
 		System.out.println(ans-1);
 		br.close();
 	}
