77a7f31f1f91264b01428cd3a22b8e7e56d08c83
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-08-02 17:37:17 +0900

    [D2] Title: 달팽이 숫자, Time: 104 ms, Memory: 16,888 KB -BaekjoonHub

---
+++ SWEA/D2/1954. 달팽이 숫자/README.md
@@ -0,0 +1,11 @@
+# [D2] 달팽이 숫자 - 1954 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PobmqAPoDFAUq) 
+
+### 성능 요약
+
+메모리: 16,888 KB, 시간: 104 ms, 코드길이: 1,272 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+@author 		Ryong
+@since 			2023. 8. 2.
+@see			https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PobmqAPoDFAUq
+@performance
+@category #
+@note
+*/
+
+public class Solution {
+	static int[] dx = {0, 1, 0, -1};
+	static int[] dy = {1, 0, -1, 0};
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void makeSnail(int n) {
+		int[][] graph = new int[n][n];
+		int x = 0, y = 0, d = 0;
+		graph[x][y] = 1;
+		int cnt = 1;
+		
+		while(cnt < n * n) {
+			int nx = x + dx[d];
+			int ny = y + dy[d];
+			
+			if(nx < 0 || ny < 0 || nx >= n || ny >= n || graph[nx][ny] != 0) {
+				d = (d+1) % 4;
+				continue;
+			}
+			graph[nx][ny] = ++cnt;
+			x = nx;
+			y = ny;
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				sb.append(graph[i][j] + " ");
+			}
+			sb.append("\n");
+		}
+		
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 1; t < T+1; t++) {
+			sb.append("#" + t + "\n");
+			makeSnail(Integer.parseInt(br.readLine()));
+		}
+		System.out.println(sb.toString());
+		br.close();
+	}
+
+}
