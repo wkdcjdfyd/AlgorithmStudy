0d0184f696cadecbe25c5c6061d1259e4646988e
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-08-03 15:19:39 +0900

    [Silver III] Title: N과 M (4), Time: 120 ms, Memory: 21060 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+@author 		Ryong
+@since 			2023. 8. 3.
+@see			https://www.acmicpc.net/problem/15652
+@performance
+@category 		#
+@note			중복조합으로 시도해봤다.
+*/
+
+public class Main {
+	static int N;
+	static int M;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void makeDupComb(int nth, int startIdx, int[] choosed) {
+		if(nth == M) {
+			for(int num : choosed) {
+				sb.append(num + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i = startIdx; i <= N; i++) {
+				choosed[nth] = i;
+				makeDupComb(nth+1, i, choosed);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		makeDupComb(0, 1, new int[M]);
+		
+		System.out.println(sb.toString());
+		br.close();
+	}
+
+}
