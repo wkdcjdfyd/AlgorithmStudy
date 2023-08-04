01439489b8f9c67b874a874d42daa1ba2f639f04
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-08-04 16:46:07 +0900

    [Silver IV] Title: 카드2, Time: 152 ms, Memory: 51184 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+@author 		Ryong
+@since 			2023. 8. 4.
+@see			https://www.acmicpc.net/problem/2164
+@performance
+@category 		#
+@note
+*/
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Deque<Integer> q = new LinkedList<>();
+		
+		for(int i = 1; i <= N; i++) {
+			q.offer(i);
+		}
+		
+		boolean turn = false;
+		int cnt = N;
+		while(cnt > 1) {
+			if(!turn) {
+				q.pollFirst();
+				cnt--;
+			}
+			else {
+				int num = q.pollFirst();
+				q.offer(num);
+			}
+			turn = !turn;
+		}
+		System.out.println(q.pollFirst());
+		br.close();
+	}
+
+}
