8d3076f2e2e64f1003c0ddd87f43c473f797ef29
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-08-09 17:20:14 +0900

    [Silver V] Title: 색종이, Time: 80 ms, Memory: 11580 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+@author 		Ryong
+@since 			2023. 8. 9.
+@see
+@performance
+@category 		#구현
+@note
+*/
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		boolean[][] paper = new boolean[100][100];
+		int cnt = 0;
+		
+		for(int n = 0; n < N; n++) {
+			st = new StringTokenizer(br.readLine());
+			int y = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			
+			for(int i = x; i < x+10; i++) {
+				for(int j = y; j < y+10; j++) {
+					if(!paper[i][j]) {
+						paper[i][j] = true;
+						cnt++;
+					}
+				}
+			}
+		}
+		System.out.println(cnt);
+		br.close();
+	}
+
+}
