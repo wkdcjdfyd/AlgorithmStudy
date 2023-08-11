bbc7da7d2f9ccf1a1b48a3309685079fa2ade860
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-08-11 12:17:20 +0900

    [Silver V] Title: 스네이크버드, Time: 84 ms, Memory: 11840 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+@author 		Ryong
+@since 			2023. 8. 11.
+@see			https://www.acmicpc.net/problem/16435
+@performance
+@category 		#그리디
+@note
+*/
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		int[] h = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			h[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(h);
+		
+		for(int i = 0; i < N; i++) {
+			if(h[i] > L) {
+				break;
+			}
+			L++;
+		}
+		System.out.println(L);
+		br.close();
+	}
+
+}
