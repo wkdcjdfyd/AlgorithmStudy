34b287c6764ad86e3195c7298ea914a475973929
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-08-16 14:11:43 +0900

    [Gold IV] Title: 월드컵, Time: 80 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Gold/6987. 월드컵/월드컵.java
@@ -0,0 +1,111 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/**
+@author 		Ryong
+@since 			2023. 8. 16.
+@see			https://www.acmicpc.net/problem/6987
+@performance	
+@category 		#
+@note			
+*/
+
+public class Main {
+	static int[][] result;
+	static ArrayList<int[]> game = new ArrayList<>();
+	static boolean ans;
+	
+	public static void makeComb(int nth, int startIdx, int[] choosed) {
+		if(nth == choosed.length) {
+			game.add(new int[] {choosed[0], choosed[1]});
+			return;
+		}
+		for(int i = startIdx; i < 7; i++) {
+			choosed[nth] = i;
+			makeComb(nth+1, i+1, choosed);
+		}
+	}
+	
+	public static boolean isAllZero() {
+		for(int i = 1; i < 7; i++) {
+			for(int j = 1; j < 4; j++) {
+				if(result[i][j] != 0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public static void search(int nth) {
+		if(nth == game.size()) {
+			if(isAllZero()) {
+				ans = true;
+			}
+			return;
+		}
+		int[] now = game.get(nth);
+		
+		// 해당 게임이 이겼을 때
+		if(result[now[0]][1] > 0 && result[now[1]][3] > 0) {
+			result[now[0]][1]--;
+			result[now[1]][3]--;
+			search(nth+1);
+			result[now[0]][1]++;
+			result[now[1]][3]++;
+		}
+		
+		// 해당 게임이 비겼을 때
+		if(result[now[0]][2] > 0 && result[now[1]][2] > 0) {
+			result[now[0]][2]--;
+			result[now[1]][2]--;
+			search(nth+1);
+			result[now[0]][2]++;
+			result[now[1]][2]++;
+		}
+		
+		// 해당 게임이 졌을 때
+		if(result[now[0]][3] > 0 && result[now[1]][1] > 0) {
+			result[now[0]][3]--;
+			result[now[1]][1]--;
+			search(nth+1);
+			result[now[0]][3]++;
+			result[now[1]][1]++;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		makeComb(0, 1, new int[2]);
+		
+		for(int t = 0; t < 4; t++) {
+			st = new StringTokenizer(br.readLine());
+			result = new int[7][4];
+			
+			for(int i = 1; i < 7; i++) {
+				for(int j = 1; j < 4; j++) {
+					result[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			ans = false;
+			search(0);
+			
+			if(ans) {
+				sb.append(1 + " ");
+			}
+			else {
+				sb.append(0 + " ");
+			}
+		}
+			
+		System.out.println(sb.toString());
+		br.close();
+	}
+
+}
