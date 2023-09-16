22122fd788f30fde53ecf26379a50e2bb9f45430
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-09-16 23:19:26 +0900

    [Silver III] Title: N과 M (7), Time: 648 ms, Memory: 256180 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * @author 			Ryong
+ * @since 			2023. 9. 16.
+ * @see  			https://www.acmicpc.net/problem/15656
+ * @performance 	
+ * @category 		#백트래킹
+ * @note 			
+ */
+
+public class Main {
+	static int N, M;
+	static int[] num;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void makePerm(int nth, int[] choosed) {
+		if(nth == M) {
+			for(int n : choosed) {
+				sb.append(n + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i = 0; i < N; i++) {
+			choosed[nth] = num[i];
+			makePerm(nth+1, choosed);
+		}
+	}
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		num = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(num);
+		
+		makePerm(0, new int[M]);
+		
+		System.out.println(sb.toString());
+		br.close();
+	}
+
+}
