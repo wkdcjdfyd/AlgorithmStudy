2c6527e32649f5ff5ab74f4ad3aaeb61f493e598
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-10-04 15:13:56 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 121 ms, Memory: 23,724 KB -BaekjoonHub

---
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int N, L;
+	static int[][] graph;
+	static int[] dx = {1, 0};
+	static int[] dy = {0, 1};
+	
+	public static boolean isRoad(int startX, int startY, int dir) {
+		int x = startX;
+		int y = startY;
+		int downCnt = 0;
+		int nowSameCnt = 1;
+		
+		while(true) {
+			int nx = x + dx[dir];
+			int ny = y + dy[dir];
+			
+			if(nx < 0 || ny < 0 || nx >= N || ny >= N) break;
+			if(Math.abs(graph[x][y] - graph[nx][ny]) > 1) return false;
+			
+			if(graph[x][y] == graph[nx][ny]) {
+				if(downCnt > 0) {
+					if(++downCnt == L) {
+						downCnt = 0;
+						nowSameCnt = 0;
+					}
+				}
+				else {
+					nowSameCnt++;
+				}
+			}
+			// 내리막
+			else if (graph[x][y] > graph[nx][ny]){
+				if(downCnt > 0) return false;
+				if(L == 1) {
+					downCnt = 0;
+					nowSameCnt = 0;
+				}
+				else {
+					downCnt = 1;
+					nowSameCnt = 1;
+				}
+			}
+			// 오르막
+			else {
+				if(nowSameCnt < L) return false;
+				if(downCnt > 0) return false;
+				nowSameCnt = 1;
+			}
+			x = nx;
+			y = ny;
+		}
+		if(downCnt > 0) return false;
+		
+		return true;
+	}
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 1; t < T+1; t++) {
+		
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			N = Integer.parseInt(st.nextToken());
+			L = Integer.parseInt(st.nextToken());
+			graph = new int[N][N];
+			
+			for(int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0; j < N; j++) {
+					graph[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			int ans = 0;
+			for(int i = 0; i < N; i++) {
+				if(isRoad(i, 0, 1)) ans++;
+				if(isRoad(0, i, 0)) ans++;
+			}
+			
+			sb.append("#").append(t).append(" ").append(ans).append("\n");
+		}
+		System.out.print(sb);
+		br.close();
+	}
+
+}
