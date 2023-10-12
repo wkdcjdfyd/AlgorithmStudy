d04d5dda5ec6522b4fc950f96201ce693567c2de
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-10-12 14:40:47 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 76 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -5,10 +5,10 @@ import java.util.StringTokenizer;
 
 /**
 @author 		Ryong
-@since 			2023. 8. 22.
-@see			https://www.acmicpc.net/problem/17070
+@since 			2023. 10. 12.
+@see			https://www.acmicpc.net/problem/17069
 @performance	
-@category 		#dfs
+@category 		#dp
 @note			
 */
 
@@ -17,64 +17,48 @@ public class Main {
 	static int[] dy = {1, 1, 0};
 	static int N;
 	static int[][] graph;
-	static boolean[][] visited;
-	static int ans = 0;
-	
-	public static void dfs(int x, int y, int state) {
-		if(x == N-1 && y == N-1) {
-			ans++;
-			return;
-		}
-		if(x == N-1 && state == 2) {
-			return;
-		}
-		if(y == N-1 && state == 0) {
-			return;
-		}
-		
-		for(int d = 0; d < dx.length; d++) {
-			if(Math.abs(state - d) == 2) {
-				continue;
-			}
-			int nx = x + dx[d];
-			int ny = y + dy[d];
-			
-			if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny]) {
-				continue;
-			}
-			if(graph[nx][ny] == 1) {
-				continue;
-			}
-			if(d == 1) {
-				if(graph[nx-1][ny] == 1 || graph[nx][ny-1] == 1) {
-					continue;
-				}
-			}
+	static long[][][] dp;
 
-			visited[nx][ny] = true;
-			dfs(nx, ny, d);
-			visited[nx][ny] = false;
-		}
-	}
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		N = Integer.parseInt(br.readLine());
-		graph = new int[N][N];
-		visited = new boolean[N][N];
+		graph = new int[N+1][N+1];
+		dp = new long[3][N+1][N+1];
 		
-		for(int i = 0; i < N; i++) {
+		for(int i = 1; i < N+1; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < N; j++) {
+			for(int j = 1; j < N+1; j++) {
 				graph[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+
+		dp[0][1][2] = 1;
+		for(int i = 1; i < N+1; i++) {
+			for(int j = 1; j < N+1; j++) {
+				if((i == 1 && j == 2) || graph[i][j] == 1) {
+					continue;
+				}
+				for(int state = 0; state < 3; state++) {
+					if(state == 0) {
+						dp[state][i][j] = dp[0][i][j-1] + dp[1][i][j-1];
+					}
+					else if(state == 1) {
+						if(graph[i-1][j] == 1 || graph[i][j-1] == 1) continue;
+						dp[state][i][j] = dp[0][i-1][j-1] + dp[1][i-1][j-1] + dp[2][i-1][j-1];
+					}
+					else {
+						dp[state][i][j] = dp[1][i-1][j] + dp[2][i-1][j];
+					}
+				}
+			}
+		}
 		
-		visited[0][0] = true;
-		visited[0][1] = true;
-		dfs(0, 1, 0);
+		long ans = 0;
+		for(int i = 0; i < 3; i++) {
+			ans += dp[i][N][N];
+		}
 		
 		System.out.println(ans);
 		br.close();
