cbce72292c00f16fedb0e232a8f190343e98d73f
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2024-02-12 16:09:23 +0900

    [Gold IV] Title: 페그 솔리테어, Time: 104 ms, Memory: 14804 KB -BaekjoonHub

--- 백준/Gold/9207. 페그 솔리테어/페그 솔리테어.java
+++ 백준/Gold/9207. 페그 솔리테어/페그 솔리테어.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
 @author         Ryong
@@ -19,67 +17,43 @@ public class Main {
     static final int W = 9;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
+    static char[][] graph;
     static int pinNum;
     static int moveNum;
 
-    public static void dfs(char[][] now, int pin, int move) {
-        List<int[]> pos = new ArrayList<>();
-        for (int i = 0; i < H; i++){
-            for(int j = 0; j < W; j++){
-                if(now[i][j] == 'o') {
-                    for(int d = 0; d < dx.length; d++){
-                        if (check(i, j, d, now)){
-                            pos.add(new int[]{i, j, d});
+    public static void dfs(int x, int y, int move, int pin) {
+        if(pin < pinNum){
+            pinNum = pin;
+            moveNum = move;
+        }
+        for(int d = 0; d < dx.length; d++){
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+
+            if(isValid(nx, ny) && graph[nx][ny] == 'o'){
+                int nnx = nx + dx[d];
+                int nny = ny + dy[d];
+                if(isValid(nnx, nny) && graph[nnx][nny] == '.'){
+                    graph[x][y] = '.';
+                    graph[nx][ny] = '.';
+                    graph[nnx][nny] = 'o';
+
+                    for(int i = 0; i < H; i++){
+                        for(int j = 0; j < W; j++){
+                            if(graph[i][j] == 'o') {
+                                dfs(i, j, move+1, pin-1);
+                            }
                         }
                     }
+                    graph[x][y] = 'o';
+                    graph[nx][ny] = 'o';
+                    graph[nnx][nny] = '.';
                 }
             }
         }
-
-        if(pin < pinNum){
-            moveNum = move;
-            pinNum = pin;
-        }
-
-        for(int[] n: pos){
-            dfs(move(n[0], n[1], n[2], now), pin-1, move+1);
-        }
     }
 
-    public static char[][] move(int x, int y, int d, char[][] old){
-        char[][] now = new char[H][W];
-        for(int i = 0; i < H; i++){
-            for(int j = 0; j < W; j++){
-                now[i][j] = old[i][j];
-            }
-        }
-        now[x][y] = '.';
-
-        int nx = x + dx[d];
-        int ny = y + dy[d];
-        now[nx][ny] = '.';
-
-        int nnx = nx + dx[d];
-        int nny = ny + dy[d];
-        now[nnx][nny] = 'o';
-
-        return now;
-    }
-
-    public static boolean check(int x, int y, int d, char[][] graph){
-        int nx = x + dx[d];
-        int ny = y + dy[d];
-
-        if(!isValid(nx, ny)) return false;
-        if(graph[nx][ny] != 'o') return false;
-
-        int nnx = nx + dx[d];
-        int nny = ny + dy[d];
-        if(!isValid(nnx, nny)) return false;
-        return graph[nnx][nny] == '.';
-    }
-
-    public static boolean isValid(int x, int y){
+    public static boolean isValid(int x, int y) {
         return 0 <= x && x < H && 0 <= y && y < W;
     }
 
@@ -89,21 +63,27 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
 
         for (int t = 0; t < T; t++) {
-            char[][] graph = new char[H][W];
-            pinNum = 0;
-            moveNum = 0;
+            graph = new char[H][W];
+            int originalPinNum = 0;
 
             for (int i = 0; i < H; i++) {
                 String s = br.readLine();
                 for(int j = 0; j < W; j++){
                     graph[i][j] = s.charAt(j);
-                    if(graph[i][j] == 'o') pinNum++;
+                    if(graph[i][j] == 'o') originalPinNum++;
                 }
             }
-
+            pinNum = originalPinNum;
+            moveNum = 0;
             if(t != T-1) br.readLine();
 
-            dfs(graph, pinNum, 0);
+            for(int i = 0; i < H; i++){
+                for(int j = 0; j < W; j++){
+                    if(graph[i][j] == 'o'){
+                        dfs(i, j, 0, originalPinNum);
+                    }
+                }
+            }
 
             sb.append(pinNum).append(" ").append(moveNum).append("\n");
         }
